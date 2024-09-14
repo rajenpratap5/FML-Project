b9edab23fe1e18952af5fe5998aa08a251a1b31f
Rajendra Singh <rajenpratap5@gmail.com>
2024-09-14 23:32:02 +0530

    Template and baasic setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Rajendra Singh",
+    author_email="rajenpratap5@gmail.com",
+    packages=find_packages()
+)

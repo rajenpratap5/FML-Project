793fa4550c3053035e537c46c6d3b6f42c18f182
Rajendra Singh <rajenpratap5@gmail.com>
2024-09-19 04:52:05 +0530

    Data Ingestion

--- MLOps/logger/__init__.py
+++ MLOps/logger/__init__.py
@@ -1,20 +1,30 @@
 import logging
-import os
-
-from from_root import from_root
-from datetime import datetime
-
-LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
-
-log_dir = 'logs'
-
-logs_path = os.path.join(from_root(), log_dir, LOG_FILE)
-
-os.makedirs(log_dir, exist_ok=True)
 
 
-logging.basicConfig(
-    filename=logs_path,
-    format="[ %(asctime)s ] %(name)s - %(levelname)s - %(message)s",
-    level=logging.DEBUG,
-)
+class CustomLogger:
+    
+    def __init__(self, logger_name, log_level=logging.DEBUG):
+        self.logger_name = logger_name
+        self.log_level = log_level
+        self.logger = self.create_logger()
+        
+    def create_logger(self):    
+        logger = logging.getLogger(name=self.logger_name)
+        file_handler = logging.FileHandler(filename='log_info.log')
+        formatter = logging.Formatter(fmt="%(asctime)s-%(name)s--%(levelname)s :: %(message)s")
+
+        # add formatter to file handler
+        file_handler.setFormatter(formatter)
+        # set level of file handler
+        file_handler.setLevel(self.log_level)
+        
+        # add handler to logger
+        logger.addHandler(file_handler)
+        # set level of logger
+        logger.setLevel(self.log_level)
+        
+        return logger
+        
+    def log_message(self,message):
+        # log the message in file
+        self.logger.debug(message)
